3e1116568594f2cad53a49cbf0c05d50c1b55913
Valyay <evgesha.valyaev@gmail.com>
2021-09-23 23:59:19 +0300

    Add pagination

--- src/main/java/com/tutorials/demo/repository/TutorialRepository.java
+++ src/main/java/com/tutorials/demo/repository/TutorialRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tutorials.demo.model.Tutorial;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-    List<Tutorial> findByPublished(boolean published);
-
-    List<Tutorial> findByTitleContaining(String title);
+    Page<Tutorial> findByPublished(boolean published, Pageable pageable);
+    Page<Tutorial> findByTitleContaining(String title, Pageable pageable);
 }
