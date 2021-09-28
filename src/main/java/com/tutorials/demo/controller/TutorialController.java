4c842a071c6b1fe32024449f65a5b3b5971972b9
Valyay <evgesha.valyaev@gmail.com>
2021-09-28 21:26:21 +0300

    Add sorting

--- src/main/java/com/tutorials/demo/controller/TutorialController.java
+++ src/main/java/com/tutorials/demo/controller/TutorialController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 
 import com.tutorials.demo.exception.ResourceNotFoundException;
 import com.tutorials.demo.model.Tutorial;
@@ -33,20 +35,45 @@ public class TutorialController {
     @Autowired
     TutorialRepository tutorialRepository;
 
+    private Sort.Direction getSortDirection(String direction) {
+        if (direction.equals("asc")) {
+            return Sort.Direction.ASC;
+        } else if (direction.equals("desc")) {
+            return Sort.Direction.DESC;
+        }
+
+        return Sort.Direction.ASC;
+    }
+
     @GetMapping("/tutorials")
     public ResponseEntity<Map<String, Object>> getAllTutorials(
             @RequestParam(required = false) String title,
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "3") int size
+            @RequestParam(defaultValue = "3") int size,
+            @RequestParam(defaultValue = "id,desc") String[] sort
     ) {
+        List<Order> orders = new ArrayList<Order>();
+
+        if (sort[0].contains(",")) {
+            // will sort more than 2 fields
+            // sortOrder="field, direction"
+            for (String sortOrder : sort) {
+                String[] _sort = sortOrder.split(",");
+                orders.add(new Order(getSortDirection(_sort[1]), _sort[0]));
+            }
+        } else {
+            // sort=[field, direction]
+            orders.add(new Order(getSortDirection(sort[1]), sort[0]));
+        }
+
         List<Tutorial> tutorials = new ArrayList<Tutorial>();
-        Pageable paging = PageRequest.of(page, size);
+        Pageable pagingSort = PageRequest.of(page, size, Sort.by(orders));
 
         Page<Tutorial> pageTuts;
         if (title == null)
-            pageTuts = tutorialRepository.findAll(paging);
+            pageTuts = tutorialRepository.findAll(pagingSort);
         else
-            pageTuts = tutorialRepository.findByTitleContaining(title, paging);
+            pageTuts = tutorialRepository.findByTitleContaining(title, pagingSort);
 
         tutorials = pageTuts.getContent();
 
--- src/main/java/com/tutorials/demo/repository/TutorialRepository.java
+++ src/main/java/com/tutorials/demo/repository/TutorialRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tutorials.demo.model.Tutorial;
-
+import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -13,4 +13,5 @@ import org.springframework.data.domain.Pageable;
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
     Page<Tutorial> findByPublished(boolean published, Pageable pageable);
     Page<Tutorial> findByTitleContaining(String title, Pageable pageable);
+    List<Tutorial> findByTitleContaining(String title, Sort sort);
 }
